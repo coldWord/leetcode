d9d57c7ef3c2903a121ae2cf8007869e5311c014
Zhou Hao <hao7cc@gmail.com>
2019-11-04 00:17:33 +0800

    [add] Directory of Tree

---
+++ Tree/Tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Tree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { 
+            val = x; 
+            left = null;
+            right = null;
+        }
+    }
+
+    TreeNode root;
+
+    Tree(TreeNode root) {
+        this.root = root;
+    }
+
+    public void preOrder(TreeNode node) {
+        if (node != null) {
+            System.out.println(node.val);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+
+        Tree t = new Tree(node1);
+        t.preOrder(node1);
+    }
+}
