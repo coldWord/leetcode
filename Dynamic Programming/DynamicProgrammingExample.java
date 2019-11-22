2d001aaa131b59a1b4f818506cdd52332c2176de
Zhou Hao <hao7cc@gmail.com>
2019-11-22 23:44:47 +0800

    add test for dynamic programming

--- Dynamic Programming/DynamicProgrammingExample.java
+++ Dynamic Programming/DynamicProgrammingExample.java
@@ -21,7 +21,33 @@ class DynamicProgrammingExample {
         return backup[n];
     }
 
+    public int fibDecBottomToTop(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        
+        DynamicProgrammingExample ex = new DynamicProgrammingExample();
+        int n = 20;
+        long  startTime = System.currentTimeMillis();
+        System.out.println("result = " + ex.fibDec(n));
+        long endTime = System.currentTimeMillis();
+        System.out.println("runtime ：" + (endTime - startTime) + "ms"); 
+        int[] backup = new int[n+1];
+        startTime = System.currentTimeMillis();
+        System.out.println("result = " + ex.fibDecWithBackup(backup, n));
+        endTime = System.currentTimeMillis();
+        System.out.println("runtime ：" + (endTime - startTime) + "ms"); 
+        startTime = System.currentTimeMillis();
+        System.out.println("result = " + ex.fibDecBottomToTop(n)); 
+        endTime = System.currentTimeMillis();
+        System.out.println("runtime ：" + (endTime - startTime) + "ms"); 
     }
 }
